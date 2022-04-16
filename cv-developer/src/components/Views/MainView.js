b2048b37f405450012697c979ab3a872ef120e65
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-04-16 13:14:25 +0200

    update project

---
+++ cv-developer/src/components/Views/MainView.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Header} from "../Header/Header";
+import {PageContent} from "../PageContent/PageContent";
+import {Footer} from "../Footer/Footer";
+
+export const MainView = () => {
+
+
+  return (
+    <div id="root">
+      <Header/>
+      <PageContent/>
+      <Footer/>
+
+    </div>
+  )
+}
